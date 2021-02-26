d0e2a546dfa4e52026cb2b1e4390e05bccd6428e
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-02-27 07:50:40 +0800

    Update vector.cpp

--- tool/vector.cpp
+++ tool/vector.cpp
@@ -5,3 +5,13 @@ vector<int>
   => arr[1]
   => arr.clear()//清空
   
+  vector<int> arr = {5, 4, 1, 7, 3, 8, 9, 10, 6, 2};
+  sort(arr.begin(), arr.begin()+10);//升序
+  sort(v.begin(), v.end());//升序
+
+  bool mycompare(int a, int b) {
+    return a > b; // 降序排列
+}
+    int arr[] = {5, 4, 1, 7, 3, 8, 9, 10, 6, 2};
+    std::vector<int> v(arr, arr+10);
+    std::sort(v.begin(), v.end(), mycompare);// 降序排列
