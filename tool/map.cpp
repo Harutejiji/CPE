8193b6b78ee5204e1b68d36e425c9ce915c06e2c
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-02-27 07:47:26 +0800

    Update and rename clear map.cpp to map.cpp

--- tool/map.cpp
+++ tool/map.cpp
@@ -5,3 +5,5 @@ mapinfo.clear();
  
 //用map.erase(map.begin(),map.end())比较好  清空元素
 gTaskDeal.matrix_info.erase(gTaskDeal.matrix_info.begin(),gTaskDeal.matrix_info.end());
+
+map<string, int, greater<string>> test; //指定引數3為greater<string> 降冪排列
