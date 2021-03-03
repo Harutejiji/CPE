f63388d6a93f50ab98f717ea63e76af5e7621a53
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-03-03 10:58:54 +0800

    Update UVA 10591.cpp

--- UVA 10591.cpp
+++ UVA 10591.cpp
@@ -1,27 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-	int T, n, m, Case = 0;
+	long long T, n, m, Case = 0;
 	cin >> T;
 	while(T--) {
-		int temp;
 		Case++;
 		cin >> n;
-		m = pow(n, 2);
-		string s = to_string(m);
-		while(m) {
-			temp = 0;
-			s = "";
+		string s = to_string(n);
+		while(Case) {
+			long long temp = 0;
 			for(int i=0; i<s.size(); i++) {
-				temp += pow(s[i] - '0', 2);
+				temp += ((s[i] - '0') * (s[i] - '0'));
 			}
 			s = to_string(temp);
-			cout << s;
-			if ((s[0] - '0') == n) break;
-			if ((s[0] - '0') == 1) break;
+			if (s.size() == 1) {
+				if ((s[0] - '0') == n) break;
+				else if ((s[0] - '0') == 1) break;				
+			}
 		}
-		//if ((s[0]-'0') == n) cout << "Case #" << Case << " :" << n << " is an Unhappy number.\n";
-		//else cout << "Case #" << Case << " :" << n << " is a Happy number.\n";
+		if ((s[0] - '0') == n) cout << "Case #" << Case << ": " << n << " is an Unhappy number.\n";
+		else cout << "Case #" << Case << ": " << n << " is a Happy number.\n";
 	}
 	return 0;
 }
