94b98a96ed55d80248beefd386bf66dc70d5e941
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-03-04 09:38:45 +0800

    Update UVA 10591.cpp

--- UVA 10591.cpp
+++ UVA 10591.cpp
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-	long long T, n, m, Case = 0;
+	long long T, n, Case = 0;
 	cin >> T;
 	while(T--) {
 		Case++;
 		cin >> n;
 		string s = to_string(n);
+		string d = s;
 		while(Case) {
 			long long temp = 0;
 			for(int i=0; i<s.size(); i++) {
 				temp += ((s[i] - '0') * (s[i] - '0'));
 			}
 			s = to_string(temp);
-			if (s.size() == 1) {
-				if ((s[0] - '0') == n) break;
-				else if ((s[0] - '0') == 1) break;				
-			}
+			if (s == d) break;
+			if (s == "1") break;
 		}
-		if ((s[0] - '0') == n) cout << "Case #" << Case << ": " << n << " is an Unhappy number.\n";
+		if (s == d) cout << "Case #" << Case << ": " << n << " is an Unhappy number.\n";
 		else cout << "Case #" << Case << ": " << n << " is a Happy number.\n";
 	}
 	return 0;
