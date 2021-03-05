bd3eda9f54c931819e0961ee858a6e41b83425bb
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-03-05 08:48:15 +0800

    Create sort.cpp

---
+++ tool/sort.cpp
@@ -0,0 +1,8 @@
+sort(arr, arr+10);//升序
+sort(arr, arr+10, greater<int>());//降序
+
+less<type>()    //从小到大排序 <
+greater<type>()  //从大到小排序 >
+less_equal<type>()  //  <=
+gtater_equal<type>()//  >=
+//这四种函数
