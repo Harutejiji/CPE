44c50b8658de459eafbb8c51e6963e4d7a34c1b3
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-03-05 08:55:44 +0800

    Update UVA 10057.cpp

--- UVA 10057.cpp
+++ UVA 10057.cpp
@@ -6,23 +6,9 @@ int main() {
 		int n[t], count = 0;
 		for(int i=0; i<t; i++) cin >> n[i];
 		sort(n, n+t);
-		if (t % 2) {
-			int mid = n[t/2];
-			for(int i=0; i<t; i++) if (n[i] == mid) count++;
-			printf("%d %d %d\n", mid, count, 1);
-			continue;
-		}
-		else if(t % 2 == 0){
-			int mid = n[t/2];
-			int midmin = n[t/2-1];
-			for(int i=midmin; i<=mid; i++) {
-				for(int j=0; j<t; j++) {
-					if(i == n[j]) count++;
-				}
-			}
-			printf("%d %d %d\n", midmin, count, mid-midmin+1);
-			continue;
-		}
+		int mid = n[(t-1)/2];
+		for(int i=0; i<t; i++) if (mid == n[i] || n[t/2]== n[i]) count++;
+		printf("%d %d %d\n", mid, count, n[t/2] - mid + 1);
 	}
 	return 0;
-}	
+}
