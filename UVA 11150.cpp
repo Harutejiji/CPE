676cda44e6dc0fdbbddcae43e2b27064f42d8aff
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-03-06 08:53:13 +0800

    Update UVA 11150.cpp

--- UVA 11150.cpp
+++ UVA 11150.cpp
@@ -13,15 +13,15 @@ int main() {
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-  int n;
-  while(cin >> n) {
-    int sum = n;
-    while(n >= 3) {
-      sum += n/3;
-      n = n%3 + n/3;
-    }
-    if(n == 2) sum++;
-    cout << sum << endl;
-  }
-  return 0;
+	int n;
+	while(cin >> n) {
+		int sum = n;
+		while(n >= 3) {
+			sum += n/3;
+			n = n%3 + n/3;
+    		}
+		if(n == 2) sum++;
+		cout << sum << endl;
+	}
+  	return 0;
 }
