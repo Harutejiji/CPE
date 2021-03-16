9d95f12b6610986b7763e07ca6a349beb1070e76
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-03-16 10:37:43 +0800

    Create UVA 458.cpp

---
+++ UVA 458.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>   
+using namespace std;
+int main() {
+	string s;
+	while(getline(cin, s)) {
+		for(int i=0; i<s.size(); i++) {
+			cout << char(s[i] - 7);
+		}
+		cout << endl;
+	}
+	return 0;
+}
