2a0459124ecacf04c2748071e82f49af129bcf38
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-03-21 21:11:36 +0800

    Create UVA 10252(*).cpp

---
+++ CPE49/UVA 10252(*).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	string s, d;
+	while(cin >> s >> d) {
+		map<char, int> ss, dd;
+		for(int i=0; i<s.size(); i++) ss[s[i]]++;
+		for(int i=0; i<d.size(); i++) dd[d[i]]++;
+		for(int i='a'; i<='z'; i++) {
+			if(ss[i] && dd[i]) {
+				for(int j=min(ss[i], dd[i]); j>0; j--) {
+					cout << (char)i;
+				}
+			}
+		}	
+		cout << endl;
+	}
+	return 0;
+}
