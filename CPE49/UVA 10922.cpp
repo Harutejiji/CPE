2b45cc1d1a7098fef8b89ca21c44f5b50be1736c
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-03-23 15:26:06 +0800

    Update UVA 10922.cpp

--- CPE49/UVA 10922.cpp
+++ CPE49/UVA 10922.cpp
@@ -1,25 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-	string s, l;
-	int n, sum;
+	string s;
+	int sum;
 	while(cin >> s && s != "0") {
-		int de = 0;
-		bool flag = false;
-		l = s;
-		while(s != "0") {
-			if (flag) break;
-			n = 0;
-			for(int i=0; i<s.size(); i++) {
-				n += s[i] - '0';
+		int c = 0;
+		string l = s;
+		while(l != "0") {
+			sum = 0;
+			for(int i=0; i<l.size(); i++) {
+				sum += l[i]-'0';
 			}
-			if(n % 9) break;
-			s = to_string(n);
-			de++;
-			if(s.size() == 1) flag = true;
+			if(sum % 9) break;
+			l = to_string(sum);
+			c++;
+			if(l.size() == 1) break;
 		}
-		if (n % 9) cout << l << " is not a multiple of 9.\n";
-		else cout << l << " is a multiple of 9 and has 9-degree " << de << ".\n";
-	}
+		if(sum % 9) cout << s << " is not a multiple of 9.\n";
+		else cout << s << " is a multiple of 9 and has 9-degree " << c << ".\n";
+	}	
 	return 0;
 }
