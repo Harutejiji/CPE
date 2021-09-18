fc7b5d3918bb48b452eb4f5a97d3cf0977213635
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-09-18 09:45:39 +0800

    Update UVA 100 .cpp

--- CPE49/UVA 100 .cpp
+++ CPE49/UVA 100 .cpp
@@ -6,17 +6,13 @@ int main() {
 		cout << n << " " << m << " ";
 		int maxi = 0;
 		for(int i=min(n, m); i<=max(m, n); i++) {
-			int count = 1, temp = i;
+			int cycle = 1, temp = i;
 			while(temp > 1) {
-				if(temp % 2) {
-					temp = (3 * temp) + 1;
-					count++;
-				}else {
-					temp /= 2;
-					count++;
-				}
+				if(temp % 2) temp = 3 * temp + 1;
+				else temp /= 2;
+				cycle++;
 			}
-			maxi = max(maxi, count);
+			maxi = max(maxi, cycle);
 		}
 		cout << maxi << endl;
 	}
