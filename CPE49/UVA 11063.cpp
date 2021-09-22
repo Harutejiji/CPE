ac3c4b39e2392cc6570737890c4c83ffda2807ff
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-09-22 09:52:10 +0800

    Update UVA 11063.cpp

--- CPE49/UVA 11063.cpp
+++ CPE49/UVA 11063.cpp
@@ -3,19 +3,20 @@ using namespace std;
 int main() {
 	int t, c = 0;
 	while(cin >> t) {
+		bool flag = true;
 		int arr[t];
-		bool flag[20001] = {false}, ans = true;
 		for(int i=0; i<t; i++) {
 			cin >> arr[i];
 		}
-		for(int i=0; i<t; i++) {
-			for(int j=i+1; j<t; j++) {
-				if(flag[arr[i] + arr[j]]) ans = false;
-				flag[arr[i] + arr[j]] = true;
+		map <int, int> m;
+		for(int i=0; i<t-1; i++) {
+			for(int j=i+1; j<t ; j++) {
+				m[arr[i]+arr[j]]++;
 			}
 		}
-		if(ans) printf("Case #%d: It is a B2-Sequence.\n\n", ++c);
-		else printf("Case #%d: It is not a B2-Sequence.\n\n", ++c);
+		for(auto i:m)  if(i.second > 1) flag = false;//cout << i.first << " " << i.second << endl;
+		if(flag) cout << "Case #" << ++c  << ": It is a B2-Sequence.\n\n";
+		else cout << "Case #" << ++c  << ": It is not a B2-Sequence.\n\n";
 	}
 	return 0;
 }
