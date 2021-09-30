9ba5217b349c3267f8b31b834798a46f55fa8924
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-09-30 09:45:50 +0800

    Update UVA 389.cpp

--- UVA 389.cpp
+++ UVA 389.cpp
@@ -9,12 +9,21 @@ int main() {
 	while(cin >> s >> n >> m) {
 		int temp = 0;
 		for(int i=0; i<s.size(); i++) {
-			temp += (s[i] - '0') * pow(n, s.size() - 1 - i);
+			if (n == 10) {
+				temp = stoi(s);
+				break;
+			}
+			if(s[i] == 'A') temp += 10 * pow(n, s.size() - 1 - i);
+			else if(s[i] == 'B') temp += 11 * pow(n, s.size() - 1 - i);
+			else if(s[i] == 'C') temp += 12 * pow(n, s.size() - 1 - i);
+			else if(s[i] == 'D') temp += 13 * pow(n, s.size() - 1 - i);
+			else if(s[i] == 'E') temp += 14 * pow(n, s.size() - 1 - i);
+			else if(s[i] == 'F') temp += 15 * pow(n, s.size() - 1 - i);
+			else temp += (s[i] - '0') * pow(n, s.size() - 1 - i);
 		}
 		if (m > 10) {
 			v.clear();
 			string d = "";
-			int ans;
 			while(temp) {
 				v.push_back(temp%m);
 				temp /= m;
@@ -24,12 +33,12 @@ int main() {
 				if(v[i] >= 10) d += h[v[i] - 10];
 				else d += (char)v[i]+'0';
 			}
+			if(d.size()>7) d.erase(0, d.size()-7);
 			printf("%07s\n" , d.c_str());
 		}
 		else if (m <= 10) {
 			b.clear();
 			string d = "";
-			int ans;
 			while(temp) {
 				b.push_back(temp%m);
 				temp /= m;
@@ -38,6 +47,7 @@ int main() {
 			for(int i=0; i<b.size(); i++) {
 				d += b[i]+'0';
 			}
+			if(d.size()>7) d.erase(0, d.size()-7);
 			printf("%07s\n" , d.c_str());
 		}
 	}
