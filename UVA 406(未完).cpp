8770c0635d99d9a87b727cf112532b6fca799d2e
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-10-01 09:21:45 +0800

    Update UVA 406(未完).cpp

--- UVA 406(未完).cpp
+++ UVA 406(未完).cpp
@@ -16,22 +16,30 @@ int main() {
 	int n, m, c;
 	while(cin >> n >> m) {
 		cout << n << " " << m << ":";
-		c = primes.size()-1 ;
+		c = primes.size()- 1 ;
 		vector<int> v;
 		while(n <= primes[c]) c--;
+		v.push_back(1);// there is no '1' in primes' vector
 		for(int i=0; i <= c; i++) {
 			v.push_back(primes[i]);
 		}
-		if(v.size()+1 % 2) {
+		int start = v.size()/2 - m + 1, end = v.size()/2 + m - 1;
+		if(!(v.size()%2)) start--;
+		if(start < 1) start = 1;
+		if(end > v.size()) end = v.size();
+		for(int i=start; i<=end; i++) {
+			cout << " " << v[i];
+		}
+		/*if(v.size() % 2) {
 			//int l = (int)v.size() / 2;
 			for(int i=1; i<=2*m-1; i++) {
-				cout << v[i] << " ";
+			//	cout << v[i] << " ";
 			}
 		}else {
 			for(int i=1; i<=2*m; i++) {
 				//cout << v[i] << " ";
 			}
-		}
+		}*/
 		cout << endl << endl;
 	}
 	return 0;
