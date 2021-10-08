a8acebd24e5d4be5d60e553eb25cade14d2a35d2
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-10-08 09:21:29 +0800

    Update UVA 10812.cpp

--- CPE49/UVA 10812.cpp
+++ CPE49/UVA 10812.cpp
@@ -1,13 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-	long long t, n, m;
+	int t, n, m, x, y;
 	cin >> t;
 	while(cin >> n >> m) {
-		long long x, y;
-		x = (n + m) / 2;
-		y = n - x;
-		if(y > 0 && x > 0 && x - y == m ) cout << x << " " << y << endl;
+		if((n+m) % 2) {
+			cout << "impossible\n";
+			continue;
+		}
+		x =(n + m) / 2;
+		y = x - m;
+		if(x >= 0 && y >= 0 && x - y == m) cout << x << " " << y << endl;
 		else cout << "impossible\n";
 	}
 	return 0;
