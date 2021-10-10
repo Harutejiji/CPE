f8bc97542c8410d2b4d3502c37e7fcfae0b700de
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-10-10 20:03:46 +0800

    Create UVA 10473ver2.cpp

---
+++ UVA 10473ver2.cpp
@@ -0,0 +1,20 @@
+/*#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	string s;
+	stringstream ss;
+	int num;
+	while(cin >> s && s[0] != '-') {
+		ss.clear();
+		ss.str("");
+		ss << s;
+		if(s[1] == 'x') {
+			ss >> hex >> num;
+			printf("%d\n", num);
+		} else {
+			ss >> dec >> num;
+			printf("0x%X\n", num);
+		}
+	}
+	return 0;
+}*/
