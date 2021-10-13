2ace52ff451fa46b8a5e440723cb4c4eea40b30c
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-10-13 09:01:24 +0800

    Update UVA 10281(未完).cpp

--- UVA 10281(未完).cpp
+++ UVA 10281(未完).cpp
@@ -3,22 +3,40 @@ using namespace std;
 int main() {
 	string a;
 	stringstream aa;
-	int h, m, sec, v, hh, mm, ssec, sum;
+	int h, m, sec, v, hh, mm, ssec, sum, i = 0;
+	double d;
 	while(getline(cin, a)) {
+		i++;
 		char b;
 		aa.clear();
 		aa.str("");
 		aa << a;
-		if(a[8] == ' ') {
-			aa >> h >> b >> m >> b >> sec >> v;
+		if(i == 1 || i == 4 || i == 6) {
+			if(i == 4) {
+				aa >> h >> b >> m >> b >> sec;
+				d = ((h*60) + m + (sec/60)) * v / 60;
+				aa >> v;
+			}else aa >> h >> b >> m >> b >> sec >> v;
 		}else {
 			cout << a << " ";
 			aa >> hh >> b >> mm >> b >> ssec;
 			sum = ((hh*60) + mm + (ssec/60)) - ((h*60) + m + (sec/60));
-			double s = v * sum / 60;
+			double s = (v * sum / 60) + d;
 			printf("%.2lf km\n", s);
 		}
 	}
 	return 0;
 }
-		
+/*cout << a << " ";
+			aa >> hh >> b >> mm >> b >> ssec;
+			sum = ((hh*60) + mm + (ssec/60)) - ((h*60) + m + (sec/60));
+			double s = (v * sum / 60) + d;
+			printf("%.2lf km\n", s);
+			
+			
+			if(i == 4) {
+				aa >> h >> b >> m >> b >> sec;
+				d = ((h*60) + m + (sec/60)) * v / 60;
+				aa >> v;
+			}else aa >> h >> b >> m >> b >> sec >> v;*/
+			
