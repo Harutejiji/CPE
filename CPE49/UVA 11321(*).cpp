7d1ce75e084161f2a4549a2e0e5a3468a4cc52a9
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-10-13 11:09:21 +0800

    Update UVA 11321(*).cpp

--- CPE49/UVA 11321(*).cpp
+++ CPE49/UVA 11321(*).cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m;
+/*bool cmp(long long a, long long b) {
+	if(a % m != b % m) return a % m < b % m;
+	if(abs(a % m) != abs(b % m)) return a % 2;
+	if(a % 2 && b % 2) return a > b;
+	else if ((a % 2 == 0) && (b % 2 == 0)) return a < b;
+	else return a % 2;
+}
+bool cmp(int i, int j) {
+    if (i % m != j % m) {
+        return i % m < j % m;
+    }
+    if ((i & 1) && (j & 1))
+        return i > j;
+    else if (!(i & 1) && !(j & 1))
+        return i < j;
+    else
+        return i & 1;
+}*/
 bool cmp(int a, int b) {
 	if(a % m != b % m) return a % m < b % m;
 	if(abs(a % 2) != abs(b % 2)) return a % 2;
