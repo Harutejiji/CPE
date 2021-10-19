f2da5b8767909d67ce575e5aaf8ad8cb0355d9fe
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2021-10-19 15:56:11 +0800

    Create tips.cpp

---
+++ tips.cpp
@@ -0,0 +1,22 @@
+const int N = 1e6; // 1000000
+bool isP[N+1];
+vector<int>primes;
+int main() {
+	for(int i=2; i<=N; i++) isP[i] = true;
+	for(int i=2; i<=N; i++) {
+		if (isP[i]) primes.push_back(i);
+		for(int j=0; j<primes.size(); j++) {
+			if (primes[j] > N / i) break;
+			isP[i*primes[j]] = false;
+			if (i % primes[j] == 0) break;
+		}
+	}
+  
+  long long _pow(long long a, long long b) {
+	if (b == 0) return 1;
+	return b % 2 ? a * _pow(a*a, b/2) : _pow(a*a, b/2);
+}
+  
+  chord = sqrt(r * r * 2 - 2 * r * r * cos(pi*a/180));
+  
+  https://github.com/Harutejiji/CPE/blob/a52c4421d5c3e68360e10ababc84ee37f8a7276c/CPE49/UVA%20**10056(math).cpp
