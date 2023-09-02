eccc3e640f51e24a3cb11de8fb675db1efa0482c
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2023-09-02 11:46:57 +0800

    Update UVA 10281.cpp

--- UVA 10281(未完).cpp
+++ UVA 10281(未完).cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main() {
 	string a;
 	stringstream aa;
-	int h, m, sec, v, hh, mm, ssec, sum, i = 0;
-	double d;
+	int h, m, sec, v, hh, mm, ssec, sum, i = 0;//105
+	double d = 0.0;
 	while(getline(cin, a)) {
 		i++;
 		char b;
@@ -13,30 +13,20 @@ int main() {
 		aa << a;
 		if(i == 1 || i == 4 || i == 6) {
 			if(i == 4) {
+				hh = h;
+				mm = m;
+				ssec = sec;
 				aa >> h >> b >> m >> b >> sec;
-				d = ((h*60) + m + (sec/60)) * v / 60;
+				d = abs(((h*3600) + (m*60) + (sec)) - ((hh*3600) + (mm*60) + (ssec))) * v / 3600.0;
 				aa >> v;
 			}else aa >> h >> b >> m >> b >> sec >> v;
 		}else {
 			cout << a << " ";
 			aa >> hh >> b >> mm >> b >> ssec;
-			sum = ((hh*60) + mm + (ssec/60)) - ((h*60) + m + (sec/60));
-			double s = (v * sum / 60.0) + d; // 當寫/60.0時會全部除到小數點後
+			sum = abs(((hh*3600) + (mm*60) + (ssec)) - ((h*3600) + (m*60) + (sec)));
+			double s = (v * sum / 3600.0) + d; // 當寫"/60.0"時會全部除到小數點後
 			printf("%.2lf km\n", s);
 		}
 	}
 	return 0;
 }
-/*cout << a << " ";
-			aa >> hh >> b >> mm >> b >> ssec;
-			sum = ((hh*60) + mm + (ssec/60)) - ((h*60) + m + (sec/60));
-			double s = (v * sum / 60) + d;
-			printf("%.2lf km\n", s);
-			
-			
-			if(i == 4) {
-				aa >> h >> b >> m >> b >> sec;
-				d = ((h*60) + m + (sec/60)) * v / 60;
-				aa >> v;
-			}else aa >> h >> b >> m >> b >> sec >> v;*/
-			
