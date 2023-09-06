94fd1d27472f0226dcd812fb8e2938d193770166
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2023-09-06 11:10:22 +0800

    Update UVA 10310.cpp

--- UVA 10310.cpp
+++ UVA 10310.cpp
@@ -1,27 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-	int h;
-	vector< pair<double, double>> v;
-	while(cin >> h) {
-		bool flag = false;
-		v.clear();
-		double gx, gy, dx, dy, temp1, temp2;
-		cin >> gx >> gy >> dx >> dy;
-		while(h--) {
-			cin >> temp1 >> temp2;
-			v.push_back(make_pair(temp1, temp2));
+	int n;
+	double x1, y1, x2, y2;
+	while(cin >> n) {
+		cin >> x1 >> y1 >> x2 >> y2;
+		double h1, h2;
+		double holx = 0, holy = 0, disd, disg;
+		vector <pair<double, double>> v;
+		for(int i=0; i<n; i++) {
+			cin >> h1 >> h2;
+			v.push_back(make_pair(h1, h2));
 		}
-		for(int i=0; i<v.size(); i++) {
-			if(abs(v[i].first - gx) * 2 <= abs(v[i].first - dx)) {
-				temp1 = v[i].first;
-				temp2 = v[i].first;
-				flag = true;
-				break;
-			}
+		for(int i=0; i<n; i++) {
+			holx = max(abs(x2-v[i].first), holx);
+			holy = max(abs(y2-v[i].second), holy);
 		}
-		if(flag) printf("The gopher can escape through the hole at (%.3lf,%.3lf).\n", temp1, temp2);
-		else printf("The gopher cannot escape.\n");
+		disd = sqrt(pow(holx, 2)+pow(holy, 2));
+		holx += x2, holy += y2;
+		holx -= x1, holy -= y1;
+		disg = sqrt(pow(holx, 2)+pow(holy, 2));
+		holx += x1, holy += y1;
+		if(disd/2.0 < disg) cout << "The gopher cannot escape.\n";
+		else printf("The gopher can escape through the hole at (%.3lf,%.3lf).\n", holx, holy);
 	}
 	return 0;
 }
