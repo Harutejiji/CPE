e605e3b1fb3cfc2109ee13da21e66009036ce602
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2023-10-12 10:08:44 +0800

    Update UVA 12019.cpp

--- CPE49/UVA 12019.cpp
+++ CPE49/UVA 12019.cpp
@@ -13,3 +13,17 @@ int main() {
 	}
 	return 0;
 }
+/*int main() {
+	int t, m, d;
+	cin >> t;
+	while(t--) {//6 7 1 2 3 4 5
+		int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
+		string day[7] = {"Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"};
+		cin >> m >> d;
+		for(int i=m-2; i>=0; i--) {
+			d += month[i];
+		}
+		cout << day[d%7] << endl;
+	}
+	return 0;
+}*/
