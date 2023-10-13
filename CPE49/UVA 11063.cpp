bd55fa27276f3fd6857a080230f8da8a00320598
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2023-10-13 11:54:21 +0800

    Update UVA 11063.cpp

--- CPE49/UVA 11063.cpp
+++ CPE49/UVA 11063.cpp
@@ -26,3 +26,28 @@ int main() {
 	}
 	return 0;
 }
+
+/*int main() {
+	int t, c = 0;
+	while(cin >> t) {
+		bool sum[10001] = {false}, flag = true;
+		int arr[t];
+		for(int i=0; i<t; i++) cin >> arr[i];
+		for(int i=0; i<t; i++) {
+			if(arr[i] >= arr[i+1]) {
+				flag = false;
+				break;
+			}
+			for(int j=i; j<t; j++) {
+				if(sum[arr[i] + arr[j]]){
+					flag = false;
+					break;
+				}
+				sum[arr[i] + arr[j]] = true;
+			}
+		}
+		if(flag) printf("Case #%d: It is a B2-Sequence.\n\n", ++c);
+		else printf("Case #%d: It is not a B2-Sequence.\n\n", ++c);
+	}
+	return 0;
+}*/
