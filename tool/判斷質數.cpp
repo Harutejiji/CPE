cfdbd490069f8a9210a426bbe80b700e2d695f8e
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2023-10-15 11:16:49 +0800

    Update 判斷質數.cpp

--- tool/判斷質數.cpp
+++ tool/判斷質數.cpp
@@ -1,15 +1,7 @@
-bool PRIME(int num)
-{
-    if (num == 2 || num == 3)
-    {
-        return true;
-    }
-    for (int i = 2; i < num; i++)
-    {
-        if (num % i == 0)
-        {
-            return false;
-        }
+bool PRIME(int num){
+    if (num == 2 || num == 3) return true;
+    for (int i = 2; i < num; i++) {
+        if (num % i == 0) return false;
     }
     return true;
 }
