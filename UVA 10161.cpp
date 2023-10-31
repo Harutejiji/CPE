1c681e6a8dbbd7c070da53ef650affd143d8b950
Harutejiji <79552137+Harutejiji@users.noreply.github.com>
2023-10-31 20:04:41 +0800

    Create UVA 10161.cpp

---
+++ UVA 10161.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+void deal(int n) {
+	int s = (int)sqrt(n+0.0);
+	if(s % 2) {
+		if(s*s == n) printf("%d %d\n", 1, s);
+		else if(n <= s*s+s+1) printf("%d %d\n", n-s*s, s+1);
+		else printf("%d %d\n", s+1, s+s+2-n+s*s);
+	}else{
+		if(s*s == n) printf("%d %d\n", s, 1);
+		else if(n <= s*s+s+1) printf("%d %d\n", s+1, n-s*s);
+		else printf("%d %d\n", s+s+2-n+s*s, s+1);
+	}
+}
+int main() {
+	int n;
+	while(cin >> n && n) {
+		deal(n);
+	}
+	return 0;
+}
